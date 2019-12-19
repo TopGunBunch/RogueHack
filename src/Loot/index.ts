02b1ecc914c8f7837da34204798b2bbdd126b6da
Yuri Maslennikov <yuri-mas@yandex-team.ru>
2019-12-19 20:58:21 +0300

    Simple generator of loot was implemented

---
+++ src/Loot/index.ts
@@ -0,0 +1,57 @@
+export class Index {
+    private LOOT_CATALOGUE: {
+        weapon: {
+            woodStick: {
+                name: 'Wood stick';
+                power: 1,
+                rareness: 1,
+            },
+            blade: {
+                name: 'Blade';
+                power: 3,
+                rareness: 3,
+            },
+            hatchet: {
+                name: 'Hatchet';
+                power: 5,
+                rareness: 5,
+            },
+        },
+        protection: {
+            helmet: {
+                name: 'Helmet';
+                power: 5,
+                rareness: 5,
+            },
+        },
+        food: {
+            dragonWings: {
+                name: 'Dragon wings';
+                power: 5,
+                rareness: 5,
+            }
+        },
+        different: {
+
+        }
+    };
+
+    private getRandom(max, min=0) {
+        return Math.floor(Math.random()*(max - min) + min);
+    }
+
+    private lootGenerator() {
+        const categoriesOfLoot = Object.keys(this.LOOT_CATALOGUE);
+        const catalogueCategoriesSize = categoriesOfLoot.length;
+        const chosenCategory = this.LOOT_CATALOGUE[categoriesOfLoot[this.getRandom(catalogueCategoriesSize - 1)])];
+        const lootsKeys = Object.keys(chosenCategory);
+        const lootsQuantity = lootsKeys.length;
+        const tempLoot = chosenCategory[lootsKeys[this.getRandom(lootsQuantity - 1)]];
+        const recievedLoot = {...tempLoot};
+        return recievedLoot;
+    }
+
+    public getLoot() {
+        return this.lootGenerator();
+    }
+};
